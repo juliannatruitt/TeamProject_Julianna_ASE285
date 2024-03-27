ad462955c0e170c5fffa48f6b493d570b40ae7c4
juliannatruitt <truittj3@nku.edu>
2024-03-27 08:32:00 -0400

    updated util folder removing unnecessary files

--- util/remove_all.js
+++
@@ -1,9 +0,0 @@
-const {URI} = require('./_config.js');
-const util = require('./mongodbutil0.js');
-const { argv } = require('node:process');
-
-const DATABASE = argv[2]; 
-const COLLECTION = argv[3]; 
-
-// Call the function to remove all documents
-util.removeAllDocuments(URI, DATABASE, COLLECTION);
