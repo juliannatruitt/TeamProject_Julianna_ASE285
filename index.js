67eea59c81f36a45e44b9c69be95b6825a42057e
juliannatruitt <truittj3@nku.edu>
2024-04-11 17:52:39 -0400

    another slight update

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
-app.use('/stylesheets', express.static('public/stylesheets'));
 app.use(methodOverride('_method'))
 
 app.listen(5500, function () {
