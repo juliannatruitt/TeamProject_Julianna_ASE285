91f5d9ac9e3979cca76c183837e6668682ddf096
Amaya Bryant <100623332+amayab9@users.noreply.github.com>
2024-04-11 11:15:36 -0400

    directory clean up and common_head.ejs in views files

---
+++ __tests__/pagination.test.js
@@ -0,0 +1,9 @@
+const { TodoApp } = require('../util/utility.js');
+const util = require('../util/mongodbutil.js');
+
+jest.mock('../util/mongodbutil.js');
+
+  describe('Pagination', () => {
+      
+  });
+    
